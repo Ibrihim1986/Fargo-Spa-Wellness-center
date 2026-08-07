764da749b0c67abd1626d4016679d9d957eb4910
ConnorGulbranson <156826311+ConnorGulbranson@users.noreply.github.com>
2026-08-07 18:28:57 -0500

    Add files via upload

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -14,6 +14,17 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(op
     public DbSet<Testimonial> Testimonials => Set<Testimonial>();
     public DbSet<ProviderAvailability> ProviderAvailabilities => Set<ProviderAvailability>();
 
+    // US-304/305/307/308
+    public DbSet<GiftCard> GiftCards => Set<GiftCard>();
+    public DbSet<Fee> Fees => Set<Fee>();
+    public DbSet<Transaction> Transactions => Set<Transaction>();
+    public DbSet<Waiver> Waivers => Set<Waiver>();
+
+    // US-709/713
+    public DbSet<SavedCard> SavedCards => Set<SavedCard>();
+    public DbSet<Dependent> Dependents => Set<Dependent>();
+    public DbSet<GuardianConsent> GuardianConsents => Set<GuardianConsent>();
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<User>()
@@ -55,5 +66,27 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(op
             .WithMany()
             .HasForeignKey(a => a.ProviderId)
             .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<GiftCard>()
+            .HasIndex(g => g.Code)
+            .IsUnique();
+
+        modelBuilder.Entity<Dependent>()
+            .HasOne<User>()
+            .WithMany()
+            .HasForeignKey(d => d.GuardianUserId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<GuardianConsent>()
+            .HasOne<Dependent>()
+            .WithMany()
+            .HasForeignKey(c => c.DependentId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<SavedCard>()
+            .HasOne<User>()
+            .WithMany()
+            .HasForeignKey(c => c.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
     }
 }
